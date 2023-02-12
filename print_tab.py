f1fb3427ed86dea2ddd52a3a5e93f1d21e87a1af
Dmitrij302 <113627593+Dmitrij302@users.noreply.github.com>
2023-02-12 23:43:36 +0700

    Delete print_tab.py

--- print_tab.py
+++
@@ -1,8 +0,0 @@
-def print_line():
-    print("-" * 13)
-
-def print_tab(tab):
-    print_line()
-    for i in range(3):
-        print ("|", tab[0+i*3], "|", tab[1+i*3], "|", tab[2+i*3], "|")
-        print_line()
